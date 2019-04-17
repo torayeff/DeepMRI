fec17e9a690827e6b5cab8ca8486048d78c5748f
torayeff <torayeff@gmail.com>
2019-04-17 21:41:03 +0500

    refactor Conv3DLSTMCell

--- Conv3DLSTMCell.py
+++ Conv3DLSTMCell.py
@@ -19,8 +19,7 @@ class Conv3DLSTMCell(nn.Module):
         self.stride = stride
         self.padding = padding
 
-        # input to hidden convolution for i, f, o, g all together
-        self.conv_ih = nn.Conv3d(
+        self.conv_input = nn.Conv3d(
             in_channels=input_channels,
             out_channels=4*hidden_channels,
             kernel_size=kernel_size,
@@ -29,10 +28,9 @@ class Conv3DLSTMCell(nn.Module):
             bias=bias
         )
 
-        # hidden to hidden convolution for i, f, o, g all together
         # always stride=1 (keep spatial dimensions)
         hidden_padding = (hidden_kernel_size - 1) // 2
-        self.conv_hh = nn.Conv3d(
+        self.conv_hidden = nn.Conv3d(
             in_channels=hidden_channels,
             out_channels=4*hidden_channels,
             kernel_size=hidden_kernel_size,
@@ -57,7 +55,7 @@ class Conv3DLSTMCell(nn.Module):
             hidden = (h, c)
 
         h_prev, c_prev = hidden
-        gates = self.conv_ih(input_batch) + self.conv_hh(h_prev)
+        gates = self.conv_input(input_batch) + self.conv_hidden(h_prev)
 
         # dim=0 is batch, dim=1 is channel
         i, f, o, g = gates.chunk(4, dim=1)
