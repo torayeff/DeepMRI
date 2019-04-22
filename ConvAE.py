91110a3050da8c3b9a0734f486f27e899bd10ac8
torayeff <torayeff@gmail.com>
2019-04-22 15:12:45 +0500

    Deeper encoder-decoder

--- ConvAE.py
+++ ConvAE.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 
 
-class SimpleAE(nn.Module):
+class ConvAE(nn.Module):
     def __init__(self):
         super().__init__()
 
@@ -26,14 +26,25 @@ class SimpleAE(nn.Module):
                 padding=1
             ),
             nn.ReLU(),
+            nn.MaxPool3d(kernel_size=2, stride=2),
+
+            # N x 32 x D/4 x H/4 x W/4 --> N x 32 x D/8 x H/8 x W/8
+            nn.Conv3d(
+                in_channels=32,
+                out_channels=32,
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ),
+            nn.ReLU(),
             nn.MaxPool3d(kernel_size=2, stride=2)
         )
 
         self.d_conv1 = nn.Sequential(
-            # N x 32 x D/4 x H/4 x W/4 --> N x 16 x D/2 x H/2 x W/2
+            # N x 32 x D/8 x H/8 x W/8 --> N x 32 x D/4 x H/4 x W/4
             nn.Conv3d(
                 in_channels=32,
-                out_channels=16,
+                out_channels=32,
                 kernel_size=3,
                 stride=1,
                 padding=1
@@ -42,6 +53,18 @@ class SimpleAE(nn.Module):
         )
 
         self.d_conv2 = nn.Sequential(
+            # N x 32 x D/4 x H/4 x W/4 --> N x 16 x D/2 x H/2 x W/2
+            nn.Conv3d(
+                in_channels=16,
+                out_channels=1,
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ),
+            nn.ReLU(),
+        )
+
+        self.d_conv3 = nn.Sequential(
             # N x 16 x D/2 x H/2 x W/2 --> N x 1 x D x H x W
             nn.Conv3d(
                 in_channels=16,
@@ -58,5 +81,7 @@ class SimpleAE(nn.Module):
         out = self.d_conv1(out)
         out = nn.functional.interpolate(out, scale_factor=2, mode='trilinear', align_corners=False)
         out = self.d_conv2(out)
+        out = nn.functional.interpolate(out, scale_factor=2, mode='trilinear', align_corners=False)
+        out = self.d_conv3(out)
         out = nn.functional.interpolate(out, scale_factor=2, mode='trilinear', align_corners=False)
         return out
