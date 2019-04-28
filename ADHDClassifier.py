e5ca73e6936d5949bebe2d772268afe5801a0f87
torayeff <torayeff@gmail.com>
2019-04-29 01:06:43 +0200

    dropout

--- ADHDClassifier.py
+++ ADHDClassifier.py
@@ -2,11 +2,12 @@ import torch.nn as nn
 
 
 class ADHDClassifier(nn.Module):
-    def __init__(self, feature_channels, w, h, d):
+    def __init__(self, feature_channels, w, h, d, p=0.5):
         super().__init__()
 
         self.hidden = feature_channels * w * h * d
         self.fc1 = nn.Linear(self.hidden, 512)
+        self.dropout = nn.Dropout(p=p)
         self.fc2 = nn.Linear(512, 2)
 
     def forward(self, x):
