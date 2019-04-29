499b8f40a1f0a52ac50e90a6534199502ec96f6f
torayeff <torayeff@gmail.com>
2019-04-29 12:53:19 +0200

    added num_labels

--- ADHDClassifier.py
+++ ADHDClassifier.py
@@ -2,13 +2,13 @@ import torch.nn as nn
 
 
 class ADHDClassifier(nn.Module):
-    def __init__(self, feature_channels, w, h, d, p=0.5):
+    def __init__(self, feature_channels, w, h, d, num_labels, p=0.5):
         super().__init__()
 
         self.hidden = feature_channels * w * h * d
         self.fc1 = nn.Linear(self.hidden, 512)
         self.dropout = nn.Dropout(p=p)
-        self.fc2 = nn.Linear(512, 2)
+        self.fc2 = nn.Linear(512, num_labels)
 
     def forward(self, x):
         x = x.view(-1, self.hidden)
