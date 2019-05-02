833b8cf5cdbac7dad3e4d8bfd96d7e704cf253a7
torayeff <torayeff@gmail.com>
2019-05-02 11:17:42 +0200

    encoder-decoder for DiffusionMRI

---
+++ DiffusionMRI/ConvDecoder.py
@@ -0,0 +1,46 @@
+import torch.nn as nn
+
+
+class ConvDecoder(nn.Module):
+    def __init__(self, out_channels):
+        super().__init__()
+        self.decode = nn.Sequential(
+            #  N x 64 x 19 x 22 x 19 --> N x 32 x 37 x 44 x 37
+            nn.ConvTranspose3d(
+                in_channels=64,
+                out_channels=32,
+                kernel_size=3,
+                stride=2,
+                padding=1,
+                output_padding=(0, 1, 0)
+            ),
+            nn.BatchNorm3d(32),
+            nn.ReLU(),
+
+            # N x 32 x 37 x 44 x 37 --> N x 16 x 73 x 87 x 73
+            nn.ConvTranspose3d(
+                in_channels=32,
+                out_channels=16,
+                kernel_size=3,
+                stride=2,
+                padding=1,
+                output_padding=(0, 0, 0)
+            ),
+            nn.BatchNorm3d(16),
+            nn.ReLU(),
+
+            # N x 16 x 73 x 87 x 73 --> N x 1 x 145 x 174 x 145
+            nn.ConvTranspose3d(
+                in_channels=16,
+                out_channels=out_channels,
+                kernel_size=3,
+                stride=2,
+                padding=1,
+                output_padding=(0, 1, 0)
+            ),
+            nn.BatchNorm3d(1)
+        )
+
+    def forward(self, x):
+        out = self.decode(x)
+        return out
