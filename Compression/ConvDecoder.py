e70dd8f05946ceb6f2dfca3c4c280bd65f786d41
torayeff <torayeff@gmail.com>
2019-05-12 14:16:56 +0200

    Experiment Compression

---
+++ Compression/ConvDecoder.py
@@ -0,0 +1,71 @@
+import torch.nn as nn
+
+
+class ConvTransposeDecoder(nn.Module):
+    def __init__(self, out_channels):
+        super().__init__()
+        self.decode = nn.Sequential(
+            #  N x 128 x 11 x 15 x 13 --> N x 64 x 22 x 29 x 25
+            nn.ConvTranspose3d(
+                in_channels=128,
+                out_channels=64,
+                kernel_size=3,
+                stride=2,
+                padding=1,
+                output_padding=(1, 0, 0)
+            ),
+            nn.ReLU(),
+            nn.BatchNorm3d(64),
+
+            # N x 64 x 22 x 29 x 25 --> N x 32 x 44 x 58 x 50
+            nn.ConvTranspose3d(
+                in_channels=64,
+                out_channels=32,
+                kernel_size=3,
+                stride=2,
+                padding=1,
+                output_padding=(1, 1, 1)
+            ),
+            nn.ReLU(),
+            nn.BatchNorm3d(32),
+
+
+            # N x 32 x 44 x 58 x 50 --> N x 16 x 88 x 115 x 100
+            nn.ConvTranspose3d(
+                in_channels=32,
+                out_channels=16,
+                kernel_size=3,
+                stride=2,
+                padding=1,
+                output_padding=(1, 0, 1)
+            ),
+            nn.ReLU(),
+            nn.BatchNorm3d(16),
+
+            # N x 16 x 88 x 115 x 100 --> N x out_channels x 176 x 229 x 200
+            nn.ConvTranspose3d(
+                in_channels=16,
+                out_channels=out_channels,
+                kernel_size=3,
+                stride=2,
+                padding=1,
+                output_padding=(1, 0, 1)
+            ),
+            nn.ReLU(),
+            nn.BatchNorm3d(out_channels),
+
+            # to remove checkerboard artifacts
+            # layer to remove checkerboard artifacts
+            #  N x out_channels x 176 x 229 x 200 -->   N x out_channels x 176 x 229 x 200
+            nn.Conv3d(
+                in_channels=out_channels,
+                out_channels=out_channels,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+            )
+        )
+
+    def forward(self, x):
+        out = self.decode(x)
+        return out
