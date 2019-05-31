dc9b4bb751680dd45acf9f677ab2a5617c1f2bfd
torayeff <torayeff@gmail.com>
2019-05-31 13:44:18 +0200

    scripts moved to new directory

---
+++ DiffusionMRI/scripts/create_orientation_dataset.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append('/home/agajan/DeepMRI')
+
+from deepmri import ds_utils  # noqa: E402
+
+csv_file = "/home/agajan/experiment_DiffusionMRI/tractseg_data/train.csv"
+save_dir = '/home/agajan/experiment_DiffusionMRI/tractseg_data/train/'
+ds_utils.create_orientation_dataset(csv_file, save_dir, orients=(0, 1, 2))
