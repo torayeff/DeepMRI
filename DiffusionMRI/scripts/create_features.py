cd0b9a34f4919a2c18557cb4eafc76758e79630d
torayeff <torayeff@gmail.com>
2019-06-18 12:07:35 +0200

    Checkpoint

--- DiffusionMRI/scripts/create_features.py
+++ DiffusionMRI/scripts/create_features.py
@@ -10,11 +10,14 @@ from DiffusionMRI.Conv2dAE import ConvEncoder  # noqa: E402
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")  # device
 torch.backends.cudnn.benchmark = True  # set False whenever input size varies
 
-subj_id = '789373'
 experiment_dir = '/home/agajan/experiment_DiffusionMRI/'
-orients = ['coronal']
-model_names = ["Conv2dAECoronal"]
-feature_shapes = [(174, 145, 145, 6)]
+
+subj_id = '784565'
+orients = ['axial']
+model_names = ["Conv2dAEAxial"]
+feature_shapes = [(145, 145, 174, 6)]
+epoch = 1000
+
 encoder = ConvEncoder()
 encoder.to(device)
 encoder.eval()
@@ -27,7 +30,6 @@ for i, orient in enumerate(orients):
     dataset = Datasets.OrientationDatasetChannelNorm(data_path, normalize=True, sort_fns=True, bg_zero=True)
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False, num_workers=10)
 
-    epoch = 31
     encoder_path = "{}models/{}_encoder_epoch_{}".format(experiment_dir, model_names[i], epoch)
     encoder.load_state_dict(torch.load(encoder_path))
     print("Loaded pretrained weights starting from epoch {} for {}".format(epoch, model_names[i]))
@@ -45,5 +47,5 @@ for i, orient in enumerate(orients):
             print(idx)
 
         orient_features = orient_features.numpy()
-        np.savez(os.path.join(features_save_path, '{}_features_epoch_{}.npz'.format(orient, epoch)),
+        np.savez(os.path.join(features_save_path, 'overfit_{}_features_epoch_{}.npz'.format(orient, epoch)),
                  data=orient_features)
